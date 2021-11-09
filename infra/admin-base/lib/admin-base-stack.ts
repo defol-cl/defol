d8fd6947d22fc69d572bdd56cc2be544437985ae
Simón Rodríguez <srodriguez@pentafinanciero.cl>
2021-11-08 23:00:07 -0300

    ADD: cognito trigger pre-token-generation lambda

--- infra/admin-base/lib/admin-base-stack.ts
+++ infra/admin-base/lib/admin-base-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as cognito from '@aws-cdk/aws-cognito';
+import * as iam from "@aws-cdk/aws-iam";
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as SSM from "@aws-cdk/aws-ssm";
 import { BaseStackProps } from './admin-base-stack.types';
 
@@ -7,6 +9,25 @@ export class AdminBaseStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: BaseStackProps) {
     super(scope, id, props);
     const { branch } = props;
+
+    const role = new iam.Role(this, 'role-lambda', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')
+    })
+    role.addToPolicy(new iam.PolicyStatement({
+      resources: ['*'],
+      actions: [
+        'logs:CreateLogGroup',
+        'logs:CreateLogStream',
+        'logs:PutLogEvents',
+      ]
+    }))
+
+    const preTokenLambda = new lambda.Function(this, `${id}-pre-token-generation`, {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      role,
+      code: lambda.Code.fromAsset("src/preToken"),
+      handler: 'preToken.handler'
+    })
     
     const userPool = new cognito.UserPool(this, `${id}-user-pool`, {
       userPoolName: `${id}-user-pool`,
@@ -41,6 +62,8 @@ export class AdminBaseStack extends cdk.Stack {
     const userPoolClient = userPool.addClient(`${id}-user-pool-client`, {
       userPoolClientName: `${id}-user-pool-client`
     });
+
+    userPool.addTrigger(cognito.UserPoolOperation.PRE_TOKEN_GENERATION, preTokenLambda);
     
     new SSM.StringParameter(this, `${id}-cognito-user-pool-id-parameter`, {
       parameterName: `/defol/${branch}/admin/user-pool-id`,
